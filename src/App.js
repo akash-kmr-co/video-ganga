40cb0988fa64554ca94cc0cc5024972118de98ee
Akash Kumar <akakumar@microsoft.com>
2022-12-24 17:30:18 +0530

    Changed title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacdt
         </a>
       </header>
     </div>
